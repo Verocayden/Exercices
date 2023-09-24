aea265544eb5448d439baadf1f8e610d7cf11d27
Verocayden <victoca@bell.net>
2023-09-24 17:17:00 -0400

    Exercices: team + bank_account FAITS

--- JS/Functions/team_manager/script.js
+++ JS/Functions/team_manager/script.js
@@ -5,13 +5,54 @@ const teams = [
     "Golden Knights",
     "Penguins"];
 
-
+// Problème de base
 function loadTeams() {
     const listItems = document.getElementsByClassName("team");
+    // Problème de scope avec var
+    // i existe à cause de la fermeture. À la fin de la boucle, i prend la dernière valeur du tableau.
+    // Du coup, cela donne toujours les Penguins, car c'est le dernier élément dans le tableau.
     for (var i = 0; i < listItems.length; i++) {
         listItems[i].addEventListener("click", (e) => {
             e.target.textContent = teams[i];
         });
+    }
+    document.getElementById("team-loader").disabled = true;
+}
+
+// Solution 1: Remplacer var par let (pour changer la fermeture)
+// var: Sa fermeture dépasse le bloc; elle inclut aussi la fonction.
+// var va prendre automatiquement la valeur la plus à jour de i, puis peut regarder "en-haut" du scope (et voit que i a déjà été déclaré) mais ce n'est pas forcément la bonne.
+// let: Contrairement au var, sa fermeture est juste dans le bloc (PAS LA FONCTION EN GÉNÉRAL)
+// Donc, avec let, on a le comportement désiré.
+// TLDR: var est chiant, let est bien.
+function loadTeams() {
+    const listItems = document.getElementsByClassName("team");
+        for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener("click", (e) => {
+            e.target.textContent = teams[i];
+        });
+    }
+    document.getElementById("team-loader").disabled = true;
+}
+
+// Solution 2: Avec IIFE
+// On a ajouté une fonction lambda avec son IIFE.
+// On est quand même capable de monter et le i est égal à 4.
+// Pourquoi ça fonctionne ?
+// C'est à cause de newI.
+// Pour lla fonction: On passe les paramètres par copie.
+// Si i change, newI ne change pas en même temps, car c'est une copie...
+// On n'a donc pas besoin de remonter en haut.
+// newI est une copie de i, pas une référence. Les changements de i n'affectent pas newI.
+// Dans une fonction, les paramètres sont passés par copie et non par référence.
+function loadTeams() {
+    const listItems = document.getElementsByClassName("team");
+    for (var i = 0; i < listItems.length; i++) {
+        (function (newI) {
+            listItems[newI].addEventListener("click", (e) => {
+                e.target.textContent = teams[newI];
+            });
+        })(i);
     }
     document.getElementById("team-loader").disabled = true;
 }
--- JS/Objects/bank_account/bank.js
+++ JS/Objects/bank_account/bank.js
@@ -1,4 +1,4 @@
-const BankAccount = (balance) =>
+const OldBankAccount = (balance) =>
     (function (copyBalance) {
         let balance = copyBalance; // Variable "privée"
         const createMessage = function () { // Méthode "privée"
@@ -27,14 +27,41 @@ const BankAccount = (balance) =>
     })(balance);
 
 // TODO
-// class BankAccount { }
+class BankAccount { 
+    #balance = 0; // Private variable!!
+    #createMessage() { // Syntax!!
+        console.log(`Compte créé avec un montant de ${this.#balance}$`);
+    }
+    constructor(balance) {
+        this.#balance = balance;
+        this.#createMessage(); // Call the private function method!!
+    }
+    withdraw(amount) {
+        if (this.#balance >= amount) {
+            this.#balance -= amount;
+            return this.#balance;
+        } else {
+            return "Fonds insuffisants";
+        }
+    }
+    deposit(amount) {
+        if (amount >= 0) {
+            this.#balance += amount;
+            return this.#balance;
+        } else {
+            return "Impossible de déposer un montant négatif";
+        }
+    }
+    checkBalance() { return this.#balance; }
+}
 
-const firstAccount = BankAccount(100); // "Compte créé avec un montant de 100$"
+// With classes: Add "new"
+const firstAccount = new BankAccount(100); // "Compte créé avec un montant de 100$"
 console.log(firstAccount.balance); // undefined
 console.log(firstAccount.checkBalance()); // 100
 console.log(firstAccount.withdraw(20)); // 80
 console.log(firstAccount.deposit(30)); // 110
 console.log(firstAccount.createMessage); // undefined
-const secondAccount = BankAccount(20); // "Compte créé avec un montant de 20$"
+const secondAccount = new BankAccount(20); // "Compte créé avec un montant de 20$"
 console.log(secondAccount.withdraw(30)); // "Fonds insuffisants"
 console.log(secondAccount.deposit(-20)); // "Impossible de déposer un montant négatif"
