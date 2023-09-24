39adbe181aae65ffa6f00c53711a5bb1dec764cb
Verocayden <victoca@bell.net>
2023-09-24 17:44:09 -0400

    OBJECT: Exercices faits (v.beta)

--- JS/Objects/shape/shape.js
+++ JS/Objects/shape/shape.js
@@ -4,8 +4,12 @@
     2. Implémenter la fonction describe() qui affiche la couleur et la forme d'une Shape
 */
 function Shape(type, color) {
-    this.getType = function () { return type; };
+    this.color = color;
+    this.getType = function () { return type ? this.type : type }; // !! SYNTAX
 }
+
+// !! SYNTAX 
+Shape.prototype.describe = function(){ return (this.color.toString() + " " + this.getType().toString());};
 
 const circle = new Shape('circle', 'red');
 const ellipse = Object.create(circle);
@@ -19,7 +23,10 @@ console.log(ellipse.describe()); // valeur désirée : red ellipse
     ainsi que les propriétés width, height et la fonction getArea()
 */
 function RectangleFactory(obj, width, height) {
-    return {};
+    // !!
+    return {
+        ...obj, width, height, getArea: function() { return this.width * this.height }
+    };
 }
 
 const rectangle = new Shape('square', 'blue');
@@ -32,7 +39,11 @@ console.log(square.getArea()); // 25
     Implémenter CircleMixin qui ajoute l'attribut radius et la fonction getArea() au paramètre obj
     Rappel : la surface d'un cercle = pi*r^2
 */
-function CircleMixin(obj, radius) { }
+function CircleMixin(obj, radius) { 
+    return Object.assign(obj, {
+        radius, getArea: function() { return Math.PI * this.radius * this.radius}
+    })
+}
 
 CircleMixin(circle, 1);
 console.log(circle.getArea().toFixed(2)); // 3.14
@@ -43,6 +54,8 @@ console.log(circle.getArea().toFixed(2)); // 3.14
 */
 Shape.prototype.isBigger = function (otherObj) { return this.getArea() > otherObj.getArea() };
 
+// square n'a pas le prototype shape, car on l'avait créé avec une fonction constructeur (??)
+Object.setPrototypeOf(square, Shape.prototype); // !! SYNTAX
 circle.isBigger(square);
 square.isBigger(circle);
 
